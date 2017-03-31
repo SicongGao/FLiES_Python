062ec79b04b6d31a3bcaef6a680492b7922e29c6
Sicong Gao@PC <xiaoyao628!>
2017-03-31 11:18:32 +1100

    delete fake file add line counter

---
+++ src/CountLines.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+#exts = ['.cs','.aspx','.cs','.js','.css','.ashx','.master','.ascx','.svc','.config']
+exts = ['.py']
+def read_line_count(fname):
+    count = 0
+    for file_line in open(fname).readlines():
+        count += 1
+    return count
+
+if __name__ == '__main__':
+
+    count = 0
+    fcount = 0
+    for root,dirs,files in os.walk(os.getcwd()):
+        for f in files:
+            # Check the sub directorys
+            fname = (root + '/'+ f).lower()
+            #print(fname)
+            ext = f[f.rindex('.'):]
+            try:
+                if(exts.index(ext) >= 0):
+                    fcount += 1
+                    c = read_line_count(fname)
+                    count += c
+            except:
+                pass
+
+    print('file count:' + str(fcount))
+    print('count:' + str(count))
