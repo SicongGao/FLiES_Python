80694ae5f76c0dc9a0cc75774949bc09ee1ed83f
Sicong Gao <siconggao@yahoo.com>
2017-07-14 17:00:20 +1000

    add seasonal mode

--- src/batchRun.py
+++ src/batchRun.py
@@ -1,23 +1,29 @@
 import shutil
 import os
+import pathlib
 import subprocess
 import time
 import re
 import csv
+import makeInputFile
 
 INPUT_DIR = "/Users/Gauss/Dropbox/Share_with_MAC/PhD_Studies/Model_work/Experiment_1_Alice_spring_seasonal" \
       "/materials/input_files/"
 FLIES_DIR = "/Users/Gauss/Dropbox/Share_with_MAC/FLiES/FLiES_v2.46/"
 OUTPUT_DIR = "/Users/Gauss/Dropbox/Share_with_MAC/PhD_Studies/Model_work/Experiment_1_Alice_spring_seasonal" \
-      "/materials/"
+      "/materials/FLiES_results/"
 
 
 def moveFile(fileName, inputFileName):
+    path = pathlib.Path(FLIES_DIR + fileName + ".txt")
 
-    print("Move file: " + fileName + ".txt")
-    newName = fileName + "_" + inputFileName
-    newName = newName.replace("init_", "")
-    shutil.move(FLIES_DIR + fileName + ".txt", FLIES_DIR + "/results/" + newName)
+    if path.exists():
+        print("Move file: " + fileName + ".txt")
+        newName = fileName + "_" + inputFileName
+        newName = newName.replace("init_", "")
+        shutil.move(FLIES_DIR + fileName + ".txt", OUTPUT_DIR + newName)
+    else:
+        print("can't find ", fileName)
 
 def batchRun():
 
@@ -25,17 +31,18 @@ def batchRun():
         os.mkdir(FLIES_DIR + "/results")
 
     for file in os.listdir(INPUT_DIR):
-        print("Processing inputfile: ", file)
-        command = FLIES_DIR + "flies" + " < " + INPUT_DIR + file
-        print(command)
+        if file.startswith("init"):
+            print("Processing inputfile: ", file)
+            command = FLIES_DIR + "flies" + " < " + INPUT_DIR + file
+            print(command)
 
-        process = subprocess.Popen(command, cwd=FLIES_DIR, shell=True)
-        process.wait()
-        print("\n###Finish: " + file)
-        time.sleep(2)
+            process = subprocess.Popen(command, cwd=FLIES_DIR, shell=True)
+            process.wait()
+            print("\n###Finish: " + file)
+            time.sleep(2)
 
-        moveFile("flxsum", file)
-        moveFile("brfsum", file)
+            moveFile("flxsum", file)
+            moveFile("brfsum", file)
 
     return True
 
@@ -55,7 +62,7 @@ def writeCSV(bandName, bandsReflectance):
     bandReflectance = list(bandsReflectance.get(bandName))
     bandDate = list(bandsReflectance.get(bandName + "_DATE"))
 
-    with open(OUTPUT_DIR + bandName + ".csv", "w") as file:
+    with open(OUTPUT_DIR + "../" + bandName + ".csv", "w") as file:
         wr = csv.writer(file, dialect="excel")
         wr.writerow(["DATE", bandName])
         for i in range(len(bandDate)):
@@ -76,8 +83,8 @@ def extractData():
                         "NIR_DATE": [],
                         }
 
-    for fileName in os.listdir(FLIES_DIR + "results/"):
-        print(FLIES_DIR + "results")
+    for fileName in os.listdir(OUTPUT_DIR):
+        #print(FLIES_DIR + "results")
         print(fileName)
         if (fileName[0] == '.'):
             continue
@@ -86,9 +93,10 @@ def extractData():
 
         #print(date)
         #print(band)
-        with open(FLIES_DIR + "results/" + fileName) as file:
+        with open(OUTPUT_DIR + fileName) as file:
             flag = False
             string = ""
+            #content = file.readlines()
 
             for line in file:
                 if line.startswith(" # idrc theta phi radiance_F stdev_F radiance_Q stdev_Q"):
@@ -112,6 +120,46 @@ def extractData():
     writeCSV("NIR", bandsReflectance)
 
 
-#batchRun()
-extractData()
-# print(getDate("init_RED_2012_04.txt"))
+def extractReflctance():
+
+    # fileName = "flxsum.txt"
+    for fileName in os.listdir(OUTPUT_DIR):
+        if fileName.startswith(".DS_Store"):
+            continue
+        else:
+            contents = []
+            print("File name:", fileName)
+            with open(OUTPUT_DIR + fileName, "r") as file:
+                for line in file:
+                    if line.startswith("    wl(um)      Etot     Ebeam"):
+                        string = next(file)
+                        while not string.startswith(" -- Downward radiance at the top of canopy --"):
+                            string = string.split()
+                            contents.append(string)
+                            string = next(file)
+                        break
+                file.close()
+
+            contents.pop()
+            wavelengthReflectance = []  # column name: wavelength, reflectance
+            for data in contents:
+                temp = [float(data[0]), float(data[4]) / 100]
+                wavelengthReflectance.append(temp)
+
+            # print(wavelengthReflectance)
+
+            # write to csv
+            fileName = fileName.replace(".txt", "")
+            # fileName.replace(".txt", "")
+            with open(OUTPUT_DIR + "../" + fileName + ".csv", "w") as file:
+                wr = csv.writer(file, dialect="excel")
+                wr.writerow(["Band", "Reflectance"])
+                for data in wavelengthReflectance:
+                    wr.writerow([str(data[0]), str(data[1])])
+
+                file.close()
+
+makeInputFile.makeInput_Fortran()
+batchRun()
+#extractData()
+extractReflctance()
--- src/makeInputFile.py
+++ src/makeInputFile.py
@@ -9,15 +9,16 @@ input_parameters = {
     "solar_angle": 45.0,
     "solar_elevation": 0.0,
     "zenith_angle": [100.0, 120.0, 140.0, 150.0, 160.0],
+    #"zenith_angle": [40.0],
     "azimuth_angle": [0.0, 180.0],
-    "integration_mode": 1,
+    "integration_mode": 3,  # short wavelength
     "single_wavelength": 0.82,
     "atmosphere_type": 2,  # Mid latitude summer
     "aerosol_type": 2,  # Continental average
     "AOT": 0.3,
     "cloud_type": 0,  # cloud free
     "surface_type": 2,  # 3-D canopy
-    "calculation_mode": 1,  # 3d APAR
+    "calculation_mode": 2,  # NIR
     "BRF_zenith_angles": [0.0, 10.0, 20.0, 30.0, 40.0, 50.0],
     "BRF_azimuth_angles": [0.0, 180],
     "tree_species": 2,
@@ -26,6 +27,11 @@ input_parameters = {
                            0.15, 0.1,  # floor reflectance, transmittance
                            0.1, 0.0001,  # trunk reflectance
                            0.05],  # soil reflectance
+    "optical_parameters_NIR": [0.1, 0.1,
+                               0.05, 0.05,
+                               0.15, 0.1,  # floor reflectance, transmittance
+                               0.1, 0.0001,  # trunk reflectance
+                               0.05],  # soil reflectance
     "leaf_area_density": [0.5, 0.5],
     "forest_floor_LAI": 0.3711,
     "branch_area_density": [0.5, 0.5],
@@ -43,8 +49,10 @@ BANDS_SETTING = {
     "RED": 0.65,
     "GREEN": 0.55,
     "BLUE": 0.47,
-    "NIR": 0.86,
+    "NIR": 0.80,
 }
+
+SUMMER = [1, 2, 3, 4, 9, 10, 11, 12]
 
 filePath = "C:/Users/12442063/Dropbox/Share with MAC/PhD Studies/Model work/" \
            "Experiment_1_Alice_spring_seasonal/materials/"
@@ -52,7 +60,8 @@ fileName_totalLAI = "total_LAI.csv"
 fileName_canopyLAI = "canopy_LAI.csv"
 fileName_understoryLAI = "understory_LAI.csv"
 folder_Input_Files = "input_files/"
-
+filePath = "/Users/Gauss/Dropbox/Share_with_MAC/PhD_Studies/Model_work/Experiment_1_Alice_spring_seasonal" \
+      "/materials/"
 
 def makeInput_Python():
     return True
@@ -88,6 +97,137 @@ def fill_LAI_DATA(fileName, LAIName):
 
     file.close()
 
+def writeNormalFile(file):
+    file.write(str(input_parameters.get("number_photon")) + " /* number_photon */\n")
+    file.write(str(input_parameters.get("atm_type")) + " /* atm_type */\n")
+    file.write(str(input_parameters.get("solar_angle")) + " ")
+    file.write(str(input_parameters.get("solar_elevation")) + " /* solar_angle, solar_elevation */\n")
+
+    tempData = getWholeString(input_parameters.get("zenith_angle"), True)
+    file.write(tempData + " /* zenith_angle */\n")
+
+    tempData = getWholeString(input_parameters.get("azimuth_angle"), True)
+    file.write(tempData + " /* azimuth_angle */\n")
+
+    file.write(str(input_parameters.get("integration_mode")) + " /* integration_mode */\n")
+    file.write(str(input_parameters.get("single_wavelength")) + " /* single_wavelength */\n")
+    file.write(str(input_parameters.get("atmosphere_type")) + " /* atmosphere_type */\n")
+    file.write(str(input_parameters.get("aerosol_type")) + " /* aerosol_type */\n")
+    file.write(str(input_parameters.get("AOT")) + " /* AOT */\n")
+    file.write(str(input_parameters.get("cloud_type")) + " /* cloud_type */\n")
+    file.write(str(input_parameters.get("surface_type")) + " /* surface_type */\n")
+    file.write(str(input_parameters.get("calculation_mode")) + " /* calculation_mode */\n")
+
+    tempData = getWholeString(input_parameters.get("BRF_zenith_angles"), True)
+    file.write(tempData + " /* BRF_zenith_angles */\n")
+
+    tempData = getWholeString(input_parameters.get("BRF_azimuth_angles"), True)
+    file.write(tempData + " /* BRF_azimuth_angles */\n")
+
+    file.write(str(input_parameters.get("tree_species")) + " /* tree_species */\n")
+
+    tempData = getWholeString(input_parameters.get("optical_parameters"), False)
+    file.write(tempData + " /* optical_parameters */\n")
+
+    tempData = getWholeString(input_parameters.get("leaf_area_density"), False)
+    file.write(tempData + " /* leaf_area_density */\n")
+
+    file.write(str(input_parameters.get("forest_floor_LAI")) + " /* forest_floor_LAI */\n")
+
+    tempData = getWholeString(input_parameters.get("branch_area_density"), False)
+    file.write(tempData + " /* branch_area_density */\n")
+
+    tempData = getWholeString(input_parameters.get("sbar"), False)
+    file.write(tempData + " /* sbar */\n")
+
+def writeNadirFile(file):
+    file.write(str(input_parameters.get("number_photon")) + " /* number_photon */\n")
+    file.write(str(input_parameters.get("atm_type")) + " /* atm_type */\n")
+    file.write(str(input_parameters.get("solar_angle")) + " ")
+    file.write(str(input_parameters.get("solar_elevation")) + " /* solar_angle, solar_elevation */\n")
+
+    tempData = getWholeString(input_parameters.get("zenith_angle"), True)
+    file.write(tempData + " /* zenith_angle */\n")
+
+    tempData = getWholeString(input_parameters.get("azimuth_angle"), True)
+    file.write(tempData + " /* azimuth_angle */\n")
+
+    file.write(str(input_parameters.get("integration_mode")) + " /* integration_mode */\n")
+    file.write(str(input_parameters.get("single_wavelength")) + " /* single_wavelength */\n")
+    file.write(str(input_parameters.get("atmosphere_type")) + " /* atmosphere_type */\n")
+    file.write(str(input_parameters.get("aerosol_type")) + " /* aerosol_type */\n")
+    file.write(str(input_parameters.get("AOT")) + " /* AOT */\n")
+    file.write(str(input_parameters.get("cloud_type")) + " /* cloud_type */\n")
+    file.write(str(input_parameters.get("surface_type")) + " /* surface_type */\n")
+    file.write(str(input_parameters.get("calculation_mode")) + " /* calculation_mode */\n")
+
+    # tempData = getWholeString(input_parameters.get("BRF_zenith_angles"), True)
+    # file.write(tempData + " /* BRF_zenith_angles */\n")
+    #
+    # tempData = getWholeString(input_parameters.get("BRF_azimuth_angles"), True)
+    # file.write(tempData + " /* BRF_azimuth_angles */\n")
+
+    file.write(str(input_parameters.get("tree_species")) + " /* tree_species */\n")
+
+    tempData = getWholeString(input_parameters.get("optical_parameters"), False)
+    file.write(tempData + " /* optical_parameters */\n")
+
+    tempData = getWholeString(input_parameters.get("leaf_area_density"), False)
+    file.write(tempData + " /* leaf_area_density */\n")
+
+    file.write(str(input_parameters.get("forest_floor_LAI")) + " /* forest_floor_LAI */\n")
+
+    tempData = getWholeString(input_parameters.get("branch_area_density"), False)
+    file.write(tempData + " /* branch_area_density */\n")
+
+    tempData = getWholeString(input_parameters.get("sbar"), False)
+    file.write(tempData + " /* sbar */\n")
+
+def writeShortWavelengthFile(file):
+    file.write(str(input_parameters.get("number_photon")) + " /* number_photon */\n")
+    file.write(str(input_parameters.get("atm_type")) + " /* atm_type */\n")
+    file.write(str(input_parameters.get("solar_angle")) + " ")
+    file.write(str(input_parameters.get("solar_elevation")) + " /* solar_angle, solar_elevation */\n")
+
+    tempData = getWholeString(input_parameters.get("zenith_angle"), True)
+    file.write(tempData + " /* zenith_angle */\n")
+
+    tempData = getWholeString(input_parameters.get("azimuth_angle"), True)
+    file.write(tempData + " /* azimuth_angle */\n")
+
+    file.write(str(input_parameters.get("integration_mode")) + " /* integration_mode */\n")
+    # file.write(str(input_parameters.get("single_wavelength")) + " /* single_wavelength */\n")
+    file.write(str(input_parameters.get("atmosphere_type")) + " /* atmosphere_type */\n")
+    file.write(str(input_parameters.get("aerosol_type")) + " /* aerosol_type */\n")
+    file.write(str(input_parameters.get("AOT")) + " /* AOT */\n")
+    file.write(str(input_parameters.get("cloud_type")) + " /* cloud_type */\n")
+    file.write(str(input_parameters.get("surface_type")) + " /* surface_type */\n")
+    file.write(str(input_parameters.get("calculation_mode")) + " /* calculation_mode */\n")
+
+    # tempData = getWholeString(input_parameters.get("BRF_zenith_angles"), True)
+    # file.write(tempData + " /* BRF_zenith_angles */\n")
+    #
+    # tempData = getWholeString(input_parameters.get("BRF_azimuth_angles"), True)
+    # file.write(tempData + " /* BRF_azimuth_angles */\n")
+
+    file.write(str(input_parameters.get("tree_species")) + " /* tree_species */\n")
+
+    tempData = getWholeString(input_parameters.get("optical_parameters"), False)
+    file.write(tempData + " /* optical_parameters */\n")
+
+    tempData = getWholeString(input_parameters.get("optical_parameters_NIR"), False)
+    file.write(tempData + " /* optical_parameters_NIR */\n")
+
+    tempData = getWholeString(input_parameters.get("leaf_area_density"), False)
+    file.write(tempData + " /* leaf_area_density */\n")
+
+    file.write(str(input_parameters.get("forest_floor_LAI")) + " /* forest_floor_LAI */\n")
+
+    tempData = getWholeString(input_parameters.get("branch_area_density"), False)
+    file.write(tempData + " /* branch_area_density */\n")
+
+    tempData = getWholeString(input_parameters.get("sbar"), False)
+    file.write(tempData + " /* sbar */\n")
 
 def makeInput_Fortran():
 
@@ -96,62 +236,43 @@ def makeInput_Fortran():
     fill_LAI_DATA(fileName_canopyLAI, "LAI_canopy")
     fill_LAI_DATA(fileName_understoryLAI, "LAI_understory")
 
-    for band in BANDS_SETTING:
-        i = 0
-        for iDate in LAI_DATA.get("DATE"):
-            inputFileName = "init_" + band + "_" + iDate + ".txt"
-            print(inputFileName)
-            input_parameters["single_wavelength"] = BANDS_SETTING.get(band)
-            input_parameters["leaf_area_density"] = []
-            input_parameters["leaf_area_density"].append(float(LAI_DATA.get("LAI_canopy")[i]))
-            input_parameters["leaf_area_density"].append(float(LAI_DATA.get("LAI_understory")[i]))
+    # for band in BANDS_SETTING:
+    #     i = 0
+    #     for iDate in LAI_DATA.get("DATE"):
+    #         inputFileName = "init_" + band + "_" + iDate + ".txt"
+    #         print(inputFileName)
+    #         input_parameters["single_wavelength"] = BANDS_SETTING.get(band)
+    #         input_parameters["leaf_area_density"] = []
+    #         input_parameters["leaf_area_density"].append(float(LAI_DATA.get("LAI_canopy")[i]))
+    #         input_parameters["leaf_area_density"].append(float(LAI_DATA.get("LAI_understory")[i]))
+    #
+    #         i += 1
+    #
+    #         with open(filePath + folder_Input_Files + inputFileName, 'w') as file:
+    #             writeShortWavelengthFile(file)
+    #
+    #         file.close()
 
-            i += 1
+    i = 0
+    for iDate in LAI_DATA.get("DATE"):
+        inputFileName = "init_" + iDate + ".txt"
+        print(inputFileName)
+        month = int(iDate.split("_")[1])
+        print(month)
+        if month in SUMMER:
+            input_parameters["atmosphere_type"] = 2
+        else:
+            input_parameters["atmosphere_type"] = 3
+        #input_parameters["single_wavelength"] = BANDS_SETTING.get(band)
+        input_parameters["leaf_area_density"] = []
+        input_parameters["leaf_area_density"].append(float(LAI_DATA.get("LAI_total")[i]))
+        input_parameters["leaf_area_density"].append(float(LAI_DATA.get("LAI_understory")[i]))
 
-            with open(filePath + folder_Input_Files + inputFileName, 'w') as file:
-    
-                file.write(str(input_parameters.get("number_photon")) + " /* number_photon */\n")
-                file.write(str(input_parameters.get("atm_type")) + " /* atm_type */\n")
-                file.write(str(input_parameters.get("solar_angle")) + " ")
-                file.write(str(input_parameters.get("solar_elevation")) + " /* solar_angle, solar_elevation */\n")
+        i += 1
 
-                tempData = getWholeString(input_parameters.get("zenith_angle"), True)
-                file.write(tempData + " /* zenith_angle */\n")
+        with open(filePath + folder_Input_Files + inputFileName, 'w') as file:
+            writeShortWavelengthFile(file)
 
-                tempData = getWholeString(input_parameters.get("azimuth_angle"), True)
-                file.write(tempData + " /* azimuth_angle */\n")
-
-                file.write(str(input_parameters.get("integration_mode")) + " /* integration_mode */\n")
-                file.write(str(input_parameters.get("single_wavelength")) + " /* single_wavelength */\n")
-                file.write(str(input_parameters.get("atmosphere_type")) + " /* atmosphere_type */\n")
-                file.write(str(input_parameters.get("aerosol_type")) + " /* aerosol_type */\n")
-                file.write(str(input_parameters.get("AOT")) + " /* AOT */\n")
-                file.write(str(input_parameters.get("cloud_type")) + " /* cloud_type */\n")
-                file.write(str(input_parameters.get("surface_type")) + " /* surface_type */\n")
-                file.write(str(input_parameters.get("calculation_mode")) + " /* calculation_mode */\n")
-
-                tempData = getWholeString(input_parameters.get("BRF_zenith_angles"), True)
-                file.write(tempData + " /* BRF_zenith_angles */\n")
-
-                tempData = getWholeString(input_parameters.get("BRF_azimuth_angles"), True)
-                file.write(tempData + " /* BRF_azimuth_angles */\n")
-
-                file.write(str(input_parameters.get("tree_species")) + " /* tree_species */\n")
-
-                tempData = getWholeString(input_parameters.get("optical_parameters"), False)
-                file.write(tempData + " /* optical_parameters */\n")
-
-                tempData = getWholeString(input_parameters.get("leaf_area_density"), False)
-                file.write(tempData + " /* leaf_area_density */\n")
-
-                file.write(str(input_parameters.get("forest_floor_LAI")) + " /* forest_floor_LAI */\n")
-
-                tempData = getWholeString(input_parameters.get("branch_area_density"), False)
-                file.write(tempData + " /* branch_area_density */\n")
-
-                tempData = getWholeString(input_parameters.get("sbar"), False)
-                file.write(tempData + " /* sbar */\n")
-
-            file.close()
+        file.close()
 
 makeInput_Fortran()
